1cdea6ea549750fdbadc3012f059d6942282ae38
Ka7im <kkrinatovich@gmail.com>
2024-01-28 18:49:03 +0300

    update ci and add memory router

---
+++ config/tests/componentRender/componentRender.tsx
@@ -0,0 +1,11 @@
+import { type ReactNode } from 'react'
+import { MemoryRouter } from 'react-router-dom'
+import { render } from '@testing-library/react'
+
+export function renderWithRouter (component: ReactNode) {
+  return render(
+      <MemoryRouter>
+        {component}
+      </MemoryRouter>
+  )
+}
